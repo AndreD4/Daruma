1c1a7126d43f926877c7dbd736182b407ef518f4
AndreD4 <andremai629@gmail.com>
2023-05-04 10:56:30 +0900

    console loging hit score

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Scorer : MonoBehaviour
 { 
-    
+    int hits = 0;
     void OnCollisionEnter(Collision other)
     {
-      
+      hits++;
+      Debug.Log("you have bumped into something this many times:" + hits);
     }
 }
