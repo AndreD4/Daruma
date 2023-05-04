868c87a1ea016709db0acaf10ff81c9e3a975c60
AndreD4 <andremai629@gmail.com>
2023-05-04 14:29:07 +0900

    if stat for not count friendly objects

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -6,8 +6,12 @@ public class Scorer : MonoBehaviour
 { 
     int hits = 0;
     void OnCollisionEnter(Collision other)
-    {
-      hits++;
-      Debug.Log("you have bumped into something this many times:" + hits);
+    { 
+      if (other.gameObject.tag != "Friendly" + "Finish")
+      {
+        hits++;
+        Debug.Log("you have bumped into something this many times:" + hits);
+      }
+      
     }
 }
