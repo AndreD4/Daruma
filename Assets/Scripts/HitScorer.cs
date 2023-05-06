5dbd3885024c9836bf1cdfeadd053ff8438ad2e7
AndreD4 <andremai629@gmail.com>
2023-05-06 17:28:27 +0900

    tickeing off todolist

--- Assets/Scripts/HitScorer.cs
+++ Assets/Scripts/HitScorer.cs
@@ -8,8 +8,6 @@ public class HitScorer : MonoBehaviour
     int hits = 0;
     void Start()
     {
-    //print put whole numbers once when hit into object
-    //printing out hit score into console log success
     //Still not working the way i want
     //want to print out score result after level finished
     //and keep track of the score for each level
